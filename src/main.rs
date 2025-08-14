55eb534e911d6619102fcd00d9523a45ec35fdce
Ben Potter <bp@benpot.com>
2025-08-14 14:23:30 -0400

    pin to rumpus 0.3.0-rc.0

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,110 @@
 use chrono::prelude::*;
 use clap::Parser;
 use image::ImageReader;
-use nalgebra::{Rotation3, Vector2};
+use rand::Rng;
+use rumpus::estimator::pattern_match::Searcher;
 use rumpus::prelude::*;
-use std::path::PathBuf;
+use serde::{Deserialize, Serialize};
+use sguaba::{engineering::Orientation, systems::Wgs84};
+use std::{io::Read, path::PathBuf};
+use uom::{
+    si::{
+        angle::degree,
+        f64::{Angle, Length},
+    },
+    ConstZero,
+};
 
 #[derive(Parser)]
 struct Cli {
-    image_path: PathBuf,
+    image: PathBuf,
+    params: PathBuf,
+}
+
+#[derive(Serialize, Deserialize)]
+struct SimulationParams {
+    pixel_size: Length,
+    focal_length: Length,
+    latitude: Angle,
+    longitude: Angle,
+    time: DateTime<Utc>,
     max_iters: usize,
 }
 
 fn main() {
     let args = Cli::parse();
+    let params = parse_params(&args.params).expect("readable and parsable params");
 
-    let raw_image = ImageReader::open(args.image_path)
-        .unwrap()
+    let raw_image = ImageReader::open(&args.image)
+        .expect("a valid image path")
         .decode()
-        .unwrap()
+        .expect("a supported image format")
         .into_luma8();
 
     let (width, height) = raw_image.dimensions();
-    let estimate = IntensityImage::from_bytes(width, height, &raw_image.into_raw())
-        .unwrap()
-        .rays(&RaySensor::new(
-            Vector2::new(3.45 * 0.001 * 2.0, 3.45 * 0.001 * 2.0),
-            Vector2::new(2448.0 / 2.0, 2048.0 / 2.0),
-        ))
+    let estimate = IntensityImage::from_bytes(width as u16, height as u16, &raw_image.into_raw())
+        .expect("image dimensions are even")
+        .rays(params.pixel_size, params.pixel_size)
         .estimate(PatternMatch::new(
-            Lens::new(8.0),
-            RayleighModel::new(
-                Position {
-                    lat: 44.2187,
-                    lon: -76.4747,
-                },
-                "2025-06-13T16:26:47+00:00"
-                    .parse::<DateTime<Utc>>()
-                    .unwrap(),
+            Lens::from_focal_length(params.focal_length)
+                .expect("focal length is greater than zero"),
+            SkyModel::from_wgs84_and_time(
+                Wgs84::builder()
+                    .latitude(params.latitude)
+                    .expect("latitude is between -90 and 90")
+                    .longitude(params.longitude)
+                    .altitude(Length::ZERO)
+                    .build(),
+                params.time,
             ),
-            StochasticSearch::try_new(
-                Orientation::new(Rotation3::from_euler_angles(-1.0, -1.0, 0.0)),
-                Orientation::new(Rotation3::from_euler_angles(1.0, 1.0, 360.0)),
-                rand::rng(),
-            )
-            .unwrap(),
-            args.max_iters,
+            RandomSearch::new(rand::rng()),
+            params.max_iters,
         ));
 
-    println!("yaw");
-    println!("{}", estimate.euler_angles().2.to_degrees());
+    println!("{:#?}", estimate.to_tait_bryan_angles());
+}
+
+fn parse_params(path: &PathBuf) -> Option<SimulationParams> {
+    let mut buffer = String::new();
+    std::fs::File::open(path)
+        .ok()?
+        .read_to_string(&mut buffer)
+        .ok()?;
+    toml::from_str(&buffer).ok()
+}
+
+struct RandomSearch<R> {
+    rng: R,
+}
+
+impl<R> RandomSearch<R> {
+    fn new(rng: R) -> Self {
+        Self { rng }
+    }
+}
+
+impl<R: Rng> Searcher for RandomSearch<R> {
+    type Iter = RandomSearchIter<R>;
+
+    fn orientations(self) -> Self::Iter {
+        RandomSearchIter { rng: self.rng }
+    }
+}
+
+struct RandomSearchIter<R> {
+    rng: R,
+}
+
+impl<R: Rng> Iterator for RandomSearchIter<R> {
+    type Item = Orientation<CameraEnu>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        Some(
+            Orientation::<CameraEnu>::tait_bryan_builder()
+                .yaw(Angle::new::<degree>(self.rng.random_range(0.0..360.0)))
+                .pitch(Angle::new::<degree>(self.rng.random_range(-5.0..5.0)))
+                .roll(Angle::new::<degree>(self.rng.random_range(-5.0..5.0)))
+                .build(),
+        )
+    }
 }
